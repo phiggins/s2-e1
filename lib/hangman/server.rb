476ac4f3fc1b7ef5d209a24ff146468f9e678028
phiggins <pete@peterhiggins.org>
2010-10-11 23:44:18 -0700

    Keep exceptions from being swallowed by rake.

--- lib/hangman/server.rb
+++ lib/hangman/server.rb
@@ -14,7 +14,15 @@ module Hangman
       loop do
         while request = @queue.next
           puts "processing request:\n#{request.inspect}" if @verbose
-          request.process
+
+          # Running the server in rake would swallow exceptions. I can make 
+          # sure I get verbose output this way.
+          begin
+            request.process
+          rescue Exception => e
+            $stderr.puts ["#{e} (#{e.class})", e.backtrace].join("\n")
+            raise
+          end
         end
         puts "sleeping for #{@wait} seconds." if @verbose
         sleep @wait
